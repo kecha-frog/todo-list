e832ef3f1679285893fa2f0f088541990bb8bbed
Ke4a <helpomos@gmail.com>
2022-06-24 13:40:42 +0500

    Initial Commit

---
+++ todo list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo list
+//
+//  Created by Ke4a on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
